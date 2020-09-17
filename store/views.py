fc77011c83682575614f06b93cb5108ef0cfce88
Jose Daniel Fassola <jfassola@everis.com>
2020-09-17 14:23:54 -0300

    segundo modulo

--- store/views.py
+++ store/views.py
@@ -1,17 +1,36 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 
 def store(request):
-    ctx = {}
-    return render(request, 'store/store.html', ctx)
+	products = Product.objects.all()
+	context = {'products':products}
+	return render(request, 'store/store.html', context)
+
 
 def cart(request):
-    ctx = {}
+
+    if request.user.is_authenticated:
+	    customer = request.user.customer
+	    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+	    items = order.orderitem_set.all()
+    else:
+		#Create empty cart for now for non-logged in user
+	    items = []
+	    order = {'get_cart_total':0, 'get_cart_items':0}
+    ctx = {'items' :items, 'order' :order}
     return render(request, 'store/cart.html', ctx)
 
 def checkout(request):
-    ctx = {}
+    if request.user.is_authenticated:
+	    customer = request.user.customer
+	    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+	    items = order.orderitem_set.all()
+    else:
+		#Create empty cart for now for non-logged in user
+	    items = []
+	    order = {'get_cart_total':0, 'get_cart_items':0}
+    ctx = {'items' :items, 'order' :order}
     return render(request, 'store/checkout.html', ctx)
 
 
